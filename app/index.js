f3936c15b544fb6515f07b04f7b6c95349f6279a
Pierre Martin <pierremartin.pro@gmail.com>
2017-10-29 22:44:40 +0100

    handle cache files

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
-import './css/main.scss';
+import './css/main.scss'; // load here but can be loaded in 'webpack.config.js' -> 'entry.app'
 import AppDemo from './demo/App';
 import NavigationMain from './components/navigation/navigationMain';
 import registerServiceWorker from './registerServiceWorker';
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const uglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const dev = process.env.NODE_ENV === 'dev';
 
 let cssLoaders = [
@@ -27,14 +29,16 @@ if (!dev) {
 
 
 const config = {
-  entry: {app: "./app/index.js"},
+  entry: {
+    app: ['./app/css/bootstrap.min.css', './app/index.js']
+  },
   watch: dev,
 
   output: {
     path: path.resolve(__dirname, "dist"),
     // the target directory for all output files - must be an absolute path (use the Node.js path module)
 
-    filename: "[name].js",
+    filename: dev ? "[name].js" : "[name].[chunkhash:8].js",
     // the filename template for entry chunks
 
     publicPath: "/dist/",
@@ -91,16 +95,24 @@ const config = {
 
   plugins: [
     // for create a css files:
-    new ExtractTextPlugin({filename: '[name].css', disable: dev}),
+    new ExtractTextPlugin({filename: dev ? '[name].css' : '[name].[contenthash:8].css', disable: dev})
   ],
 };
 
 
 if (!dev) {
   // Minify JS :
-  config.plugins.push(new uglifyJsPlugin({
-    sourceMap: false
-  }))
+  config.plugins.push(new uglifyJsPlugin({sourceMap: false}));
+
+  // Used for the hash name's files :
+  config.plugins.push(new ManifestPlugin());
+
+  // Used for the unusable files in the folder's build :
+  config.plugins.push(new CleanWebpackPlugin(['dist'], {
+    root: path.resolve(__dirname, './'),
+    verbose: true,
+    dry: false
+  }));
 }
 
 
